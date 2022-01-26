34fcf01e3de5f18cfb7d90d6f4e0979555aa6ce6
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2022-01-26 15:35:01 +0100

    [Linux_IIO_Network_Server] Added official IIO source code and configured build, only 'ops' functions should be missing.

---
+++ Linux_IIO_Network_Server/Sources/Main.c
@@ -0,0 +1,16 @@
+/** @file Main.c
+ * Linux IIO internet server running on a non-Linux OS.
+ * @author Adrien RICCIARDI
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+//-------------------------------------------------------------------------------------------------
+// Entry point
+//-------------------------------------------------------------------------------------------------
+int main(int argc, char *argv[])
+{
+	// TODO
+
+	return EXIT_SUCCESS;
+}
