8627b116583a861b0078c09b087e9bd5d65c5a56
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2022-11-14 07:48:11 -0800

    [Kvaser CAN error detector] Added errors accounting and displaying.

--- Kvaser_CAN_Error_Detector/Main.c
+++ Kvaser_CAN_Error_Detector/Main.c
@@ -8,6 +8,22 @@
 #include <string.h>
 
 //-------------------------------------------------------------------------------------------------
+// Private types
+//-------------------------------------------------------------------------------------------------
+/** All detectable CAN transmission errors. See CANlib canMSGERR_XXX documentation for more information. */
+typedef enum
+{
+	CAN_ERROR_ID_HW_OVERRUN,
+	CAN_ERROR_ID_SW_OVERRUN,
+	CAN_ERROR_ID_STUFF,
+	CAN_ERROR_ID_FORM,
+	CAN_ERROR_ID_CRC,
+	CAN_ERROR_ID_BIT0,
+	CAN_ERROR_ID_BIT1,
+	CAN_ERROR_IDS_COUNT
+} TCANErrorID;
+
+//-------------------------------------------------------------------------------------------------
 // Private functions
 //-------------------------------------------------------------------------------------------------
 /** Return a static string containing the error message description (like strerror()).
@@ -32,8 +48,8 @@ int main(int argc, char *argv[])
 	canHandle Handle;
 	int Channel, Return_Value = EXIT_FAILURE;
 	canStatus Result;
-	unsigned char Payload[64];
-	unsigned int Data_Length, Flags, i;
+	unsigned int Flags;
+	unsigned long Error_Counters[CAN_ERROR_IDS_COUNT] = {0}, Cumulated_Errors_Counter = 0, Received_Valid_Frames_Counter = 0;
 
 	// Check parameters
 	if (argc != 2)
@@ -88,24 +104,54 @@ int main(int argc, char *argv[])
 		goto Exit_Close_Handle;
 	}
 	
-	printf("Waiting for CAN frames ...\n");
+	printf("Waiting for CAN frames. Stop sending frames for more than 10 seconds for the program to automatically exit.\n");
 	while (1)
 	{
 		// Wait for a frame to be received
-		Result = canReadWait(Handle, NULL, Payload, &Data_Length, &Flags, NULL, 0xFFFFFFFF);
+		Result = canReadWait(Handle, NULL, NULL, NULL, &Flags, NULL, 10000); // Automatically exit program when no frame is received after 10 seconds
 		if (Result != canOK)
 		{
-			printf("Error : failed to read a CAN frame (%s).\n", GetErrorMessage(Result));
-			goto Exit_Turn_Bus_Off;
+			if (Result == canERR_TIMEOUT) printf("Timeout while waiting for CAN frames, exiting program.\n");
+			else printf("Error : failed to read a CAN frame (%s).\n", GetErrorMessage(Result));
+			break;
 		}
 
-		printf("TEST %u\n", Data_Length);
-		printf("FLAGS : 0x%08X.\n", Flags);
-		for (i = 0; i < Data_Length; i++) printf("%d = %02X\n", i, Payload[i]);
-		break;
+		// Did any error occurred ?
+		if (Flags & canMSGERR_MASK)
+		{
+			Cumulated_Errors_Counter++;
+			if (Flags & canMSGERR_HW_OVERRUN) Error_Counters[CAN_ERROR_ID_HW_OVERRUN]++;
+			if (Flags & canMSGERR_SW_OVERRUN) Error_Counters[CAN_ERROR_ID_SW_OVERRUN]++;
+			if (Flags & canMSGERR_STUFF) Error_Counters[CAN_ERROR_ID_STUFF]++;
+			if (Flags & canMSGERR_FORM) Error_Counters[CAN_ERROR_ID_FORM]++;
+			if (Flags & canMSGERR_CRC) Error_Counters[CAN_ERROR_ID_CRC]++;
+			if (Flags & canMSGERR_BIT0) Error_Counters[CAN_ERROR_ID_BIT0]++;
+			if (Flags & canMSGERR_BIT1) Error_Counters[CAN_ERROR_ID_BIT1]++;
+		}
+		else Received_Valid_Frames_Counter++;
 	}
 
-Exit_Turn_Bus_Off:
+	// Display statistics
+	printf("Received valid frames : %lu\n"
+		"Cumulated errors : %lu\n"
+		"Hardware overruns : %lu\n"
+		"Software overruns : %lu\n"
+		"Stuffing errors : %lu\n"
+		"Form errors : %lu\n"
+		"CRC errors : %lu\n"
+		"Bit 0 errors : %lu\n"
+		"Bit 1 errors : %lu\n",
+		Received_Valid_Frames_Counter,
+		Cumulated_Errors_Counter,
+		Error_Counters[CAN_ERROR_ID_HW_OVERRUN],
+		Error_Counters[CAN_ERROR_ID_SW_OVERRUN]++,
+		Error_Counters[CAN_ERROR_ID_STUFF],
+		Error_Counters[CAN_ERROR_ID_FORM],
+		Error_Counters[CAN_ERROR_ID_CRC],
+		Error_Counters[CAN_ERROR_ID_BIT0],
+		Error_Counters[CAN_ERROR_ID_BIT1]);
+
+	// Turn bus off
 	Result = canBusOff(Handle);
 	if (Result != canOK) printf("Error : failed to turn bus off (%s).\n", GetErrorMessage(Result));
 
