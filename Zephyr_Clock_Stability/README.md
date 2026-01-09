aec23c6165ae315f6a16e1da731362c827f7adb9
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2026-01-09 14:24:29 +0100

    [Zephyr clock stability] Ported the application to NCS v3.2.1.

--- Zephyr_Clock_Stability/README.md
+++ Zephyr_Clock_Stability/README.md
@@ -1,9 +1,11 @@
 # Build instructions
 
 ```
+export NCS_VERSION=v3.2.1
+
 # Enter the NCS shell
-nrfutil sdk-manager toolchain launch --ncs-version v3.1.0 --shell
-source ~/ncs/v3.1.0/zephyr/zephyr-env.sh
+nrfutil sdk-manager toolchain launch --ncs-version ${NCS_VERSION} --shell
+source ~/ncs/${NCS_VERSION}/zephyr/zephyr-env.sh
 
 # Build the firmware
 west build -p -b nrf54h20dk/nrf54h20/cpuapp .
--- Zephyr_Clock_Stability/src/main.c
+++ Zephyr_Clock_Stability/src/main.c
@@ -10,13 +10,8 @@
 // Port 9 pin 0
 #define GPIO_PIN (9 << 5)
 
-ISR_DIRECT_DECLARE(timer_interrupt_handler)
-{
-    // Invoke the handler at nRFx driver level, which in turn will invoke the event handler registered below
-    NRFX_TIMER_INST_HANDLER_GET(TIMER_INSTANCE_ID)();
-    ISR_DIRECT_PM();
-    return 1;
-}
+static nrfx_timer_t timerInstance = NRFX_TIMER_INSTANCE(NRF_TIMER_INST_GET(TIMER_INSTANCE_ID));
+static nrfx_gpiote_t gpioteInstance = NRFX_GPIOTE_INSTANCE(NRF_GPIOTE_INST_GET(GPIOTE_INSTANCE_ID));
 
 static void timerInterruptHandler(nrf_timer_event_t event_type, void *p_context)
 {
@@ -28,34 +23,28 @@ static void timerInterruptHandler(nrf_timer_event_t event_type, void *p_context)
 
 static void configureDppi(void)
 {
-	uint8_t gppiChannel;
 	uint32_t sourceEndpoint, destinationEndpoint;
-	nrfx_timer_t timerInstance = NRFX_TIMER_INSTANCE(TIMER_INSTANCE_ID);
-	nrfx_gpiote_t gpioteInstance = NRFX_GPIOTE_INSTANCE(GPIOTE_INSTANCE_ID);
-	nrfx_err_t ret;
+	nrfx_gppi_handle_t gppiHandle;
+	int ret;
 
-	sourceEndpoint = nrfx_timer_event_address_get(&timerInstance, NRF_TIMER_EVENT_COMPARE0);
-	destinationEndpoint = nrfx_gpiote_out_task_address_get(&gpioteInstance, GPIO_PIN);
+	sourceEndpoint = nrf_timer_event_address_get(timerInstance.p_reg, NRF_TIMER_EVENT_COMPARE0);
+	destinationEndpoint = nrf_gpiote_task_address_get(gpioteInstance.p_reg, NRF_GPIOTE_TASK_OUT_0);
 
-	ret = nrfx_gppi_channel_alloc(&gppiChannel);
-	if (ret != NRFX_SUCCESS)
+	ret = nrfx_gppi_conn_alloc(sourceEndpoint, destinationEndpoint, &gppiHandle);
+	if (ret != 0)
 	{
-		printk("Error : failed to allocated a GPPI channel (0x%08X).\n", ret);
+		printk("Error : failed to allocated a GPPI connection (0x%08X).\n", ret);
 		return;
 	}
-	printk("Allocated GPPI channel : %u.\n", gppiChannel);
 
-	nrfx_gppi_channel_endpoints_setup(gppiChannel, sourceEndpoint, destinationEndpoint);
-	nrfx_gppi_channels_enable(NRFX_BIT(gppiChannel));
+	nrfx_gppi_conn_enable(gppiHandle);
 }
 
 int main(void)
 {
-	nrfx_err_t ret;
+	int ret;
 	nrfx_timer_config_t timerConfiguration = NRFX_TIMER_DEFAULT_CONFIG(1000000);
-	nrfx_timer_t timerInstance = NRFX_TIMER_INSTANCE(TIMER_INSTANCE_ID);
 	uint32_t ticks;
-	static nrfx_gpiote_t gpioteInstance = NRFX_GPIOTE_INSTANCE(GPIOTE_INSTANCE_ID);
 	nrfx_gpiote_output_config_t gpioteOutputConfiguration =
 	{
 		.drive = NRF_GPIO_PIN_S0S1,
@@ -74,27 +63,27 @@ int main(void)
 	// Initialize the selected timer
 	timerConfiguration.bit_width = NRF_TIMER_BIT_WIDTH_32;
 	ret = nrfx_timer_init(&timerInstance, &timerConfiguration, timerInterruptHandler);
-	if (ret != NRFX_SUCCESS) {
+	if (ret != 0) {
 		printk("Error : failed to initialize the timer (0x%08X).", ret);
 		return -1;
 	}
 	nrfx_timer_clear(&timerInstance);
 
 	ret = nrfx_gpiote_init(&gpioteInstance, NRFX_GPIOTE_DEFAULT_CONFIG_IRQ_PRIORITY);
-	if ((ret != NRFX_SUCCESS) && (ret != NRFX_ERROR_ALREADY)) {
+	if ((ret != 0) && (ret != -EALREADY)) {
 		printk("Error : failed to initialize the GPIOTE instance (0x%08X).", ret);
 		return -1;
 	}
 	ret = nrfx_gpiote_output_configure(&gpioteInstance, GPIO_PIN, &gpioteOutputConfiguration, &gpioteTaskConfiguration);
-	if (ret != NRFX_SUCCESS) {
+	if (ret != 0) {
 		printk("Error : failed to configure the GPIOTE output (0x%08X).", ret);
 		return -1;
 	}
 	nrfx_gpiote_out_task_enable(&gpioteInstance, GPIO_PIN);
 
 	// Configure the interrupt firing frequency
-	IRQ_DIRECT_CONNECT(NRFX_IRQ_NUMBER_GET(NRF_TIMER_INST_GET(TIMER_INSTANCE_ID)), DT_IRQ(DT_NODELABEL(TIMER_DT_LABEL), priority), timer_interrupt_handler, 0);
-	ticks = nrfx_timer_us_to_ticks(&timerInstance, 2000000);
+	IRQ_CONNECT(NRFX_IRQ_NUMBER_GET(NRF_TIMER_INST_GET(TIMER_INSTANCE_ID)), DT_IRQ(DT_NODELABEL(TIMER_DT_LABEL), priority), nrfx_timer_irq_handler, &timerInstance, 0);
+	ticks = nrfx_timer_us_to_ticks(&timerInstance, 500000);
 	nrfx_timer_extended_compare(&timerInstance, NRF_TIMER_CC_CHANNEL0, ticks, NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, true);
 
 	configureDppi();
