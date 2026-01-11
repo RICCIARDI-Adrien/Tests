e5ef51d5d8f23f1b79d72995f4022324e836d7f7
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2026-01-11 18:19:17 +0100

    [Zephyr multiple USB controllers] Enabled the high speed USB controller in addition to the already enabled full speed one.

--- Zephyr_Multiple_USB_Controllers/src/main.c
+++ Zephyr_Multiple_USB_Controllers/src/main.c
@@ -1,4 +1,11 @@
 #include <zephyr/kernel.h>
+#include <zephyr/usb/usbd.h>
+
+#define USB_VID_ZEPHYR 0x2fe3
+#define USB_PID_PRODUCT 0xcaca
+
+USBD_DEVICE_DEFINE(test_usbd0, DEVICE_DT_GET(DT_NODELABEL(zephyr_udc0)), USB_VID_ZEPHYR, USB_PID_PRODUCT);
+USBD_DEVICE_DEFINE(test_usbd1, DEVICE_DT_GET(DT_NODELABEL(zephyr_udc1)), USB_VID_ZEPHYR, USB_PID_PRODUCT);
 
 int main(void)
 {
