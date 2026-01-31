7c79b3b1954df06416f40dcaf9d4e893ef403743
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2026-01-31 23:19:29 +0100

    [Zephyr Cirque trackpad] Added a callback called upon reception of a trackpad input event.

--- Zephyr_Cirque_Trackpad/src/main.c
+++ Zephyr_Cirque_Trackpad/src/main.c
@@ -1,4 +1,12 @@
+#include <zephyr/input/input.h>
 #include <zephyr/kernel.h>
+
+static void TrackpadInputCallback(struct input_event *evt, void *user_data)
+{
+	printk("code=%u, val=%d\n", evt->code, evt->value);
+}
+
+INPUT_CALLBACK_DEFINE(DEVICE_DT_GET(DT_NODELABEL(trackpad)), TrackpadInputCallback, NULL);
 
 int main(void)
 {
